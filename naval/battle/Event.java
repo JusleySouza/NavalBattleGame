b75ad4dc23886c0e3f707c954d0d781f24433db7
Jusley Souza <jusleysouza31@gmail.com>
2023-09-26 22:00:20 -0300

    Creation and implementation of the Event Class

---
+++ naval/battle/Event.java
@@ -0,0 +1,14 @@
+package naval.battle;
+
+public class Event {
+   private String message;
+
+   public Event (String message) {
+        this.message = message;   
+   }
+
+   public String getMensagem() {
+        return this.message;
+   }
+}
+
